7174a4bedb4f70beb33eb47898f6cdcdae35bfd6
AivarMinsafin <42532733+AivarMinsafin@users.noreply.github.com>
2019-02-19 18:59:23 +0300

    Add files via upload

--- 3rd_day_19feb/src/com/company/Main08EndlessCalc.java
+++ 3rd_day_19feb/src/com/company/Main08EndlessCalc.java
@@ -8,42 +8,72 @@ import java.util.Scanner;
  */
 public class Main08EndlessCalc {
     public static void main(String[] args) {
+        double out=0;
         while (0 < 1) {
-            double out = 0;
-            // System.out.println("insert the number: ");
             Scanner sc = new Scanner(System.in);
-            double a1 = sc.nextDouble();
-            if (a1 == 0) {
-                break;
-            }
-            // System.out.println("insert operation(+,-,*,/): ");
-            String op = sc.next();
-            // System.out.println("insert the next number: ");
-            double a2 = sc.nextDouble();
-            if (op.equals("+"))
+            if (out==0) {
+                double a1 = sc.nextDouble();
+                String op = sc.next();
+                double a2 = sc.nextDouble();
+                if (op.equals("+"))
 
-            {
-                out = a1 + a2;
-            } else if (op.equals("-"))
+                {
+                    out = a1 + a2;
+                } else if (op.equals("-"))
 
-            {
-                out = a1 - a2;
-            } else if (op.equals("*"))
+                {
+                    out = a1 - a2;
+                } else if (op.equals("*"))
 
-            {
-                out = a1 * a2;
-            } else if (op.equals("/"))
+                {
+                    out = a1 * a2;
+                } else if (op.equals("/"))
 
-            {
-                out = a1 / a2;
-            } else if (op.equals("^"))
+                {
+                    out = a1 / a2;
+                } else if (op.equals("^"))
 
-            {
-                out = Main09Pow.chPow(a1, a2);
+                {
+                    out = Main09Pow.chPow(a1, a2);
+                }
+
+            }else {
+                String op = sc.next();
+                double a2 = sc.nextDouble();
+                if (op.equals("+"))
+
+                {
+                    out = out + a2;
+                } else if (op.equals("-"))
+
+                {
+                    out = out - a2;
+                } else if (op.equals("*"))
+
+                {
+                    out = out * a2;
+                } else if (op.equals("/"))
+
+                {
+                    out = out / a2;
+                } else if (op.equals("^"))
+
+                {
+                    out = Main09Pow.chPow(out, a2);
+                }
             }
 
             System.out.println("result: " + out);
-            System.out.println("insert `0` to stop");
+            System.out.println("Reset result?(`y` - yes, `n` - no)");
+            String reset = sc.next();
+            if (reset.equals("y")) {
+                out = 0;
+            }
+            System.out.println("insert `s` to stop, `g` to continue");
+            String stop = sc.next();
+            if (stop.equals("s")) {
+                break;
+            }
         }
     }
 }
